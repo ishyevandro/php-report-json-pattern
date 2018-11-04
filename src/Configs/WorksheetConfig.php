d9e23a8c6e7ea09cda434363b035fa21b38f6cc1
Evandro Medeiros <ishyevandro@gmail.com>
2018-11-04 18:11:30 -0200

    Testing worksheet config

--- src/Configs/WorksheetConfig.php
+++ src/Configs/WorksheetConfig.php
@@ -2,14 +2,21 @@
 
 namespace IshyEvandro\XlsPatternGenerator\Configs;
 
+use IshyEvandro\XlsPatternGenerator\Exceptions\XlsPatternGeneratorException;
+
 /**
  * Class WorksheetConfig
  * @package IshyEvandro\XlsPatternGenerator\Configs
  */
 class WorksheetConfig
 {
+    protected $expectConfigFields = [
+        "fields",
+        "header_line",
+        "first_column"
+    ];
     /**
-     * @var
+     * @var array
      */
     protected $config;
 
@@ -20,10 +27,29 @@ class WorksheetConfig
     }
 
     /**
-     *
+     * @return bool
+     * @throws XlsPatternGeneratorException
      */
-    protected function validateConfig()
+    protected function validateConfig(): bool
     {
+        foreach ($this->expectConfigFields as $field) {
+            $this->checkField($field);
+        }
+        return true;
+    }
 
+    /**
+     * @param $field
+     * @return bool
+     * @throws XlsPatternGeneratorException
+     */
+    protected function checkField($field): bool
+    {
+        if (!array_key_exists($field, $this->config)) {
+            throw new XlsPatternGeneratorException(
+                "key [$field] not exist in sheet configuration"
+            );
+        }
+        return true;
     }
 }
--- tests/Configs/WorksheetConfigTest.php
+++ tests/Configs/WorksheetConfigTest.php
@@ -1,12 +1,55 @@
 <?php
 
 use IshyEvandro\XlsPatternGenerator\Configs\WorksheetConfig;
+use IshyEvandro\XlsPatternGenerator\Exceptions\XlsPatternGeneratorException;
 use PHPUnit\Framework\TestCase;
 
 class WorksheetConfigTest extends TestCase
 {
-    public function testAssertTrue()
+    public function testConstructWithCorrectJsonShouldNotThrowException()
     {
-        $this->assertFalse(true);
+        new WorksheetConfig($this->config());
+        $this->assertTrue(true);
+    }
+
+    public function testConstructWithJsonWithoutFieldsShouldThrownException()
+    {
+        $this->expectException(XlsPatternGeneratorException::class);
+        $json = $this->config();
+        unset($json['fields']);
+        new WorksheetConfig($json);
+    }
+
+    public function testConstructWithJsonWithoutFirstColumnShouldThrownException()
+    {
+        $this->expectException(XlsPatternGeneratorException::class);
+        $json = $this->config();
+        unset($json['first_column']);
+        new WorksheetConfig($json);
+    }
+
+    public function testConstructWithJsonWithoutHeaderLineShouldThrownException()
+    {
+        $this->expectException(XlsPatternGeneratorException::class);
+        $json = $this->config();
+        unset($json['header_line']);
+        new WorksheetConfig($json);
+    }
+
+    protected function config()
+    {
+        return json_decode(
+            '{
+                "fields": [
+                  "field_1",
+                  "field_2",
+                  "field_3",
+                  "field_4"
+                ],
+                "first_column": "A",
+                "header_line": 1
+              }',
+            true
+        );
     }
 }
