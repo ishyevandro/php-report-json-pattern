54b6b304163a02f4f973c0250d82ea5c68489ff9
Evandro Medeiros <ishyevandro@gmail.com>
2018-11-04 19:36:19 -0200

    Creating an xlsx with 40k row

--- examples/simple_xlsx_with_40000_row.php
+++ examples/simple_xlsx_with_40000_row.php
@@ -1,11 +1,11 @@
 <?php
-
 include(__DIR__ . "/../vendor/autoload.php");
+
 use IshyEvandro\XlsPatternGenerator\XlsPatternGenerator;
 
-$json = file_get_contents(__DIR__.'/simple_xlsx.json');
+$json = file_get_contents(__DIR__.'/simple_xlsx_40000_row.json');
 $data = json_decode($json, true);
 
 $processor = new XlsPatternGenerator($data);
 $processor->process();
-$processor->save(__DIR__.'/../tmp/simple.xlsx');
+$processor->save(__DIR__.'/../tmp/simple_40k_row.xlsx');
