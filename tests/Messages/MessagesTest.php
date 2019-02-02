950c776c53fbcc38b70fc08f0cbd974c9a024642
Evandro Medeiros <ishyevandro@gmail.com>
2019-02-02 13:34:55 -0200

    Correcting phpcs things

--- tests/Messages/MessagesTest.php
+++ tests/Messages/MessagesTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use IshyEvandro\XlsPatternGenerator\Messages\Messages;
 use IshyEvandro\XlsPatternGenerator\Exceptions\XlsPatternGeneratorException;
@@ -6,23 +7,26 @@ use PHPUnit\Framework\TestCase;
 
 class MessagesTest extends TestCase
 {
-    public function testMessageNotFound()
+    public function testMessageNotFound(): void
     {
         $this->expectException(XlsPatternGeneratorException::class);
-        $findMessage = "TEST_NOT_FOUND";
+        $findMessage = 'TEST_NOT_FOUND';
         $this->expectExceptionMessage("Message {$findMessage} not found");
         Messages::getMessage($findMessage);
     }
 
-    public function testGetMessageWithoutParameter()
+    public function testGetMessageWithoutParameter(): void
     {
-        $this->assertEquals(Messages::MESSAGES[Messages::CONFIG_MISSING_FIELD], Messages::getMessage(Messages::CONFIG_MISSING_FIELD));
+        $this->assertEquals(
+            Messages::MESSAGES[Messages::CONFIG_MISSING_FIELD],
+            Messages::getMessage(Messages::CONFIG_MISSING_FIELD)
+        );
     }
 
-    public function testGetMessageWithParameter()
+    public function testGetMessageWithParameter(): void
     {
-        $field = "test";
-        $expected = str_replace("{field}", $field, Messages::MESSAGES[Messages::CONFIG_MISSING_FIELD]);
+        $field = 'test';
+        $expected = str_replace('{field}', $field, Messages::MESSAGES[Messages::CONFIG_MISSING_FIELD]);
         $this->assertEquals(
             $expected,
             Messages::getMessage(Messages::CONFIG_MISSING_FIELD, [
